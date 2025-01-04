179f6ee15999e862d12e103f8ff1b2014b073401
Nitin Bisht <nbishtsni99@gmail.com>
2025-01-05 01:07:21 +0530

    Added test cases for a single number in the string

--- stringCalculator/test/stringCalculator.test.js
+++ stringCalculator/test/stringCalculator.test.js
@@ -4,3 +4,8 @@ test("add should return 0 if there is an empty string passed", ()=> {
     const res = add("");
     expect(res).toBe(0);
 });
+
+test("add should returns number for a single element in the string", () => {
+    expect(add("1")).toBe(1);
+    expect(add("2")).toBe(2);
+});
