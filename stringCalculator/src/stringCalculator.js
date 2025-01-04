1e42bb7367268f2cc97f40ff11d6f57cb516eda5
Nitin Bisht <nbishtsni99@gmail.com>
2025-01-05 01:53:44 +0530

    Added code to pass the test case to add any number separated by a comma or a new line character

--- stringCalculator/src/stringCalculator.js
+++ stringCalculator/src/stringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
     if(numbers=="") return 0;
-    const sum = numbers.split(",").reduce((total, num) => total + parseInt(num, 10), 0);
+    const sum = numbers.replace("\n", ",").split(",").reduce((total, num) => total + parseInt(num, 10), 0);
     return sum;
 }
 
