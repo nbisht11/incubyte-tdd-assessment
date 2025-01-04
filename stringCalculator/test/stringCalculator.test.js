2a0355b0376d2390856f0ac8648ed28274222a8d
Nitin Bisht <nbishtsni99@gmail.com>
2025-01-05 01:19:40 +0530

    Added test case to add two comma separated numbers in the string

--- stringCalculator/test/stringCalculator.test.js
+++ stringCalculator/test/stringCalculator.test.js
@@ -9,3 +9,8 @@ test("add should returns number for a single element in the string", () => {
     expect(add("1")).toBe(1);
     expect(add("2")).toBe(2);
 });
+
+test("add should return the sum for a two numbers separated by a comma in the string", () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("2,3")).toBe(5);
+});
