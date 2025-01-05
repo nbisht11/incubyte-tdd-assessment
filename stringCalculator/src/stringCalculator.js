b10d98cac98d5178de8875ed5546b090d975a7a1
Nitin Bisht <nbishtsni99@gmail.com>
2025-01-05 22:15:40 +0530

    Added code to pass the test case to use a custom delimeter in the add function

--- stringCalculator/src/stringCalculator.js
+++ stringCalculator/src/stringCalculator.js
@@ -1,6 +1,14 @@
 function add(numbers) {
     if(numbers=="") return 0;
-    const sum = numbers.replace("\n", ",").split(",").reduce((total, num) => total + parseInt(num, 10), 0);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+        const numbersAndDelimiter = numbers.split("\n");
+        delimiter = new RegExp(`[${numbersAndDelimiter[0].slice(2)}]`);
+        numbers = numbersAndDelimiter[1];
+    }
+
+  const numbersArray = numbers.split(delimiter);
+    const sum = numbersArray.reduce((total, num) => total + parseInt(num, 10), 0);
     return sum;
 }
 
